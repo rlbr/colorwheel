15b2ec7c66fbc4e3193563a0cc0c96e6a23b81d5
Raphael Roberts <raphael.roberts48@gmail.com>
2023-03-28 17:17:03 -0500

    Added circle crop

--- main.py
+++ main.py
@@ -14,7 +14,15 @@ def circle_center(h, n):
 
 def next_h(h):
     r = radius(h)
-    return h - r - r * .6
+    return h - r - r * 0.6
+
+
+def circle_crop(image: Image):
+    size = image.size
+    mask = Image.new("L", size, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0) + size, fill=255)
+    image.putalpha(mask)
 
 
 s = 6
@@ -28,11 +36,9 @@ for j in range(11):
     for i in range(12):
         r = radius(h)
         x, y = circle_center(h, i)
-        color = ImageColor.getrgb(
-            f"hsl({360//12 * i}, {int(100 * .5 ** j)}%, 50%)")
-        draw.ellipse(((x0 + x - r, y0 + y - r), (x0 + x + r, y0 + y + r)),
-                     color)
+        color = ImageColor.getrgb(f"hsl({360//12 * i}, {int(100 * .5 ** j)}%, 50%)")
+        draw.ellipse(((x0 + x - r, y0 + y - r), (x0 + x + r, y0 + y + r)), color)
     h = next_h(h)
 
-with open("out.png", 'wb') as im:
+with open("out.png", "wb") as im:
     img.save(im)
