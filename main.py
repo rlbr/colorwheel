fafcf8a79a2ed27a5c1e2bc0cd64a76a45b78054
Raphael Roberts <raphael.roberts48@gmail.com>
2023-03-30 13:00:26 -0500

    Started on image paste. Need to get rid of crop

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ def circle_crop(image: Image):
     image.putalpha(mask)
 
 
-s = 6
 def draw_shape(draw, x0, y0, h, i):
     r = radius(h)
     x, y = circle_center(h, i)
@@ -33,17 +32,43 @@ def draw_shape(draw, x0, y0, h, i):
     draw.ellipse(((x0 + x - r, y0 + y - r), (x0 + x + r, y0 + y + r)), color)
 
 
+def shrink(img: Image.Image, size):
+
+    return img.resize(size, Image.LANCZOS)
+
+
+def place_img(base: Image, img: Image, x0, y0, h, i):
+    working_copy = img.copy()
+    r = int(radius(h))
+    working_copy = shrink(working_copy, (r * 2, r * 2))
+    x, y = circle_center(h, i)
+    # coords = [int(v) for v in (x0 + x - r, y0 + y - r, x0 + x + r, y0 + y + r)]
+    coords = [int(v) for v in (x0 + x - r, y0 + y - r)]
+
+    circle_crop(working_copy)
+    base.paste(working_copy, coords)
+
+
+s = 3
 img = Image.new("RGBA", (1200 * s, 1200 * s))
+base = img.copy()
 draw = ImageDraw.Draw(img)
 h0 = 460 * s
 x0 = 600 * s
 y0 = 600 * s
 h = h0
+mf = Image.open("./middle_finder.jpg")
+
 for j in range(11):
     for i in range(12):
 
         draw_shape(draw, x0, y0, h, i)
+        place_img(base, mf, x0, y0, h, i)
+
     h = next_h(h)
 
 with open("out.png", "wb") as im:
     img.save(im)
+
+with open("out2.png", "wb") as im2:
+    base.save(im2)
