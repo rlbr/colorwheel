0e4ee2bf3a5401fe5c5f9700dd9fcb53464493b2
Raphael Roberts <raphael.roberts48@gmail.com>
2023-04-04 00:59:32 -0500

    Works as intended

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import math
-from PIL import Image, ImageDraw, ImageColor
+from PIL import Image, ImageDraw, ImageColor, ImageOps, ExifTags
+from load_images import image_paths
+
+
+def fix_rotation(image):
+    try:
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation] == "Orientation":
+                break
+
+        exif = image.getexif()
+
+        if exif[orientation] == 3:
+            image = image.rotate(180, expand=True)
+        elif exif[orientation] == 6:
+            image = image.rotate(270, expand=True)
+        elif exif[orientation] == 8:
+            image = image.rotate(90, expand=True)
+
+    except (AttributeError, KeyError, IndexError):
+        pass
+    return image
 
 
 def radius(h):
@@ -40,6 +61,7 @@ def shrink(img: Image.Image, size):
 def place_img(base: Image, img: Image, x0, y0, h, i):
     working_copy = img.copy()
     r = int(radius(h))
+
     working_copy = shrink(working_copy, (r * 2, r * 2))
     x, y = circle_center(h, i)
     # coords = [int(v) for v in (x0 + x - r, y0 + y - r, x0 + x + r, y0 + y + r)]
@@ -49,21 +71,31 @@ def place_img(base: Image, img: Image, x0, y0, h, i):
     base.paste(working_copy, coords, working_copy)
 
 
-s = 3
+s = 6
 img = Image.new("RGBA", (1200 * s, 1200 * s))
-base = img.copy()
+base = Image.new("RGBA", (1200 * s, 1200 * s))
 draw = ImageDraw.Draw(img)
 h0 = 460 * s
 x0 = 600 * s
 y0 = 600 * s
 h = h0
-mf = Image.open("./middle_finder.jpg")
+
 
 for j in range(11):
+    tier = j % 4
     for i in range(12):
+        print(f"j={j},i={i}")
 
+        images = image_paths[tier]
+        image_no = i % len(images)
+        image_path = images[image_no]
+
+        loaded_image = Image.open(image_path)
+        roation_fixed = fix_rotation(loaded_image)
+        gs = ImageOps.grayscale(loaded_image).convert("RGBA")
         draw_shape(draw, x0, y0, h, i)
-        place_img(base, mf, x0, y0, h, i)
+        place_img(base, gs, x0, y0, h, i)
+        loaded_image.close()
 
     h = next_h(h)
 
@@ -72,3 +104,8 @@ with open("out.png", "wb") as im:
 
 with open("out2.png", "wb") as im2:
     base.save(im2)
+
+blended = Image.blend(base, img, 0.5)
+
+with open("out3.png", "wb") as im3:
+    blended.save(im3)
